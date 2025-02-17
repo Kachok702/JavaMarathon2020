ab2cbc6a620218c53e94366aab6af0b00f4d2760
Marsel Shaykhulov <marsel-shaihulov@mail.ru>
2025-02-17 23:08:40 +0500

    Выполнил день 3, но не проходит тесты

--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -4,6 +4,20 @@ import java.util.Scanner;
 
 public class Task3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int i = 1;
 
+        while (i <= 5) {
+            double a = scanner.nextDouble();
+            double b = scanner.nextDouble();
+            if (b == 0) {
+                i++;
+                System.out.println("Деление на 0");
+                continue;
+            }
+            System.out.println(a / b);
+            i++;
+        }
     }
 }
+
